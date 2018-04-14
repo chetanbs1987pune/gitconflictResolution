05dcc0177bb0deb7c593ff4a8b81da5e107e8845
chetan <chetanbs1987pune@gmail.com>
2018-04-14 08:15:26 +0530

    commit to local

--- resolveConflictLearning/src/LearningJava/firstClass.java
+++ resolveConflictLearning/src/LearningJava/firstClass.java
@@ -3,6 +3,7 @@ package LearningJava;
 public class firstClass {
 	public void display(){
 		System.out.println("Display function added by the user");
+		System.out.println("second print statement added");
 	}
 	
 
